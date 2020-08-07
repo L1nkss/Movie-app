56ddfa56a5ae54e7acb8ce649665e0ed6193abe1
Сергей <s.kharlov@modulbank.ru>
2020-08-07 16:47:47 +0500

    test

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+
+import "./style/style.scss";
+
+const App = () => {
+  return (
+    <div>Hello</div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
