9ff48d88bba92ebacb42efc0cebff9d31e7fda54
Сергей Харлов <l1nks@inbox.ru>
2020-08-25 00:12:51 +0500

    loading

--- src/components/film-details/film-details.tsx
+++ src/components/film-details/film-details.tsx
@@ -4,9 +4,9 @@ import { TFilmDetails } from "@redux/reducers/films/types/types";
 import Spinner from "@components/spinner/spinner";
 import MenuList from "@components/menu-list/menu-list.connect";
 import Adapter from "@redux/reducers/films/utils/adapter";
+import createFilmCards from "@components/film-list/utils/utils";
 import history from "../../utils/history";
 import Service from "../../api/api";
-import createFilmCards from "@components/film-list/utils/utils";
 
 interface IFilmDetailsProps {
   loadDetails: (id: number) => void,
@@ -37,54 +37,61 @@ const FilmDetails: React.FC<IFilmDetailsProps> = (props: IFilmDetailsProps): JSX
 
     getFilmRecommendations();
   }, []);
-  console.log(filmsRecommendations);
+
+  // Загружаем новые данные, если обновился ID фильма
+  useEffect(() => {
+    props.loadDetails(id);
+  }, [id]);
+
   return (
     <>
-      <div className="film-details">
-        {loading && <Spinner />}
-        {details && !loading && (
-          <>
-            <div className="film-details__poster">
-              <img src={`https://image.tmdb.org/t/p/w342/${details.posterPath}`} alt="Изображение" />
-            </div>
-            <div className="film-details__wrapper">
-              <div className="film-details__header">
-                <div className="film-details__header-inner">
-                  <h1 className="film-details__header-text">{details.title}</h1>
-                  <p className="film-details__header-information">
-                    {details.voteAverage}
-                    {" "}
-                    /
-                    {details.runtime !== 0 ? `${details.runtime} min. /` : null }
-                    {" "}
-                    2020
+      {loading && <Spinner />}
+      {details && !loading && (
+        <>
+          <div className="film-details">
+            <>
+              <div className="film-details__poster">
+                <img src={`https://image.tmdb.org/t/p/w342/${details.posterPath}`} alt="Изображение" />
+              </div>
+              <div className="film-details__wrapper">
+                <div className="film-details__header">
+                  <div className="film-details__header-inner">
+                    <h1 className="film-details__header-text">{details.title}</h1>
+                    <p className="film-details__header-information">
+                      {details.voteAverage}
+                      {" "}
+                      /
+                      {details.runtime !== 0 ? `${details.runtime} min. /` : null }
+                      {" "}
+                      2020
+                    </p>
+                  </div>
+                  <h2 className="film-details__header-subtext">{details.tagline}</h2>
+                </div>
+                <div className="film-details__genres">
+                  <h3>Жанры</h3>
+                  <MenuList items={details.genres} noActive />
+                </div>
+                <div className="film-details__description">
+                  <h3>Описание</h3>
+                  <p>
+                    {details.overview}
                   </p>
                 </div>
-                <h2 className="film-details__header-subtext">{details.tagline}</h2>
+                <div>
+                  <button type="button" onClick={() => { history.goBack(); }}>Назад</button>
+                </div>
               </div>
-              <div className="film-details__genres">
-                <h3>Жанры</h3>
-                <MenuList items={details.genres} noActive />
-              </div>
-              <div className="film-details__description">
-                <h3>Описание</h3>
-                <p>
-                  {details.overview}
-                </p>
-              </div>
-              <div>
-                <button type="button" onClick={() => { history.goBack(); }}>Назад</button>
-              </div>
+            </>
+          </div>
+          <div>
+            <h2>Возможно вам понравится: </h2>
+            <div className="film-list">
+              {createFilmCards(filmsRecommendations)}
             </div>
-          </>
-        )}
-      </div>
-      <div>
-        <h2>Возможно вам понравится: </h2>
-        <div className="film-list">
-          {createFilmCards(filmsRecommendations)}
-        </div>
-      </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
